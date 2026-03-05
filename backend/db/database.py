9f45996378f9bbdcb074215a7cdadc6a6e7b73af
Tanay <tanayj2107@gmail.com>
2026-03-04 20:48:10 -0500

    database file done

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,1 +1,21 @@
 # Async SQLAlchemy setup — engine, session factory, base model
+from dotenv import load_dotenv
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from backend.models.review import Base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session = async_sessionmaker(engine, expire_on_commit = False)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def get_db():
+    async with async_session() as session:
+        yield session
